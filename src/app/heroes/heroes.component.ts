6a7103c37795014e6e5e158c2e48652fdfa4116b
Andrei Roman <Andrei.Roman@qubiz.local>
2019-07-10 15:42:01 +0300

    Add Service and Service Message

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-
+import {HeroService} from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,12 +10,16 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes : Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService:HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+  getHeroes():void {
+    this.heroService.getHeros().subscribe(heroes => this.heroes=heroes);
   }
 
   onSelect(hero: Hero): void {
